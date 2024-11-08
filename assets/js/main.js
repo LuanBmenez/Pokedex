28b1245907c78051a12b60e9fbc2abc35a273e2e
Luan Menezes <menezluan120@gmail.com>
2024-11-08 07:10:16 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,33 +3,75 @@ const loadMoreButton = document.getElementById("loadMoreButton");
 const limit = 20;
 let offset = 0;
 
-function loadPokemonItens(offset, limit) {
-  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const newHtml = pokemons
-      .map(
-        (pokemon) => `
-    <li class="pokemon ${pokemon.type}" >
-         <span class="number">#${pokemon.number}</span>
-         <span class="name">${pokemon.name}</span>
+// function loadPokemonItems(offset, limit) {
+//   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+//     const newHtml = pokemons
+//       .map(
+//         (pokemon) => `
+//     <li class="pokemon ${pokemon.type}" >
+//          <span class="number">#${pokemon.number}</span>
+//          <span class="name">${pokemon.name}</span>
 
-         <div class="detail">
-           <ol class="types">
-               ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
-           </ol>
-           <img
-             src="${pokemon.photo}"
-             alt="${pokemon.name}"
-           />
-         </div>
-       </li>
-   ` ).join("");
-    pokemonList.innerHTML += newHtml;
+//          <div class="detail">
+//            <ol class="types">
+//                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
+//            </ol>
+//            <img
+//              src="${pokemon.photo}"
+//              alt="${pokemon.name}"
+//            />
+//          </div>
+//        </li>
+//    ` ).join("");
+//     pokemonList.innerHTML += newHtml;
+//   });
+// }
+
+function loadPokemonItems(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    pokemons.forEach((pokemon) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("pokemon", pokemon.type);
+
+      const number = document.createElement("span");
+      number.classList.add("number");
+      number.textContent = `#${pokemon.number}`;
+
+      const name = document.createElement("span");
+      name.classList.add("name");
+      name.textContent = pokemon.name;
+
+      const detail = document.createElement("div");
+      detail.classList.add("detail");
+
+      const typesList = document.createElement("ol");
+      typesList.classList.add("types");
+
+      pokemon.types.forEach((type) => {
+        const typeItem = document.createElement("li");
+        typeItem.classList.add("type", type);
+        typeItem.textContent = type;
+        typesList.appendChild(typeItem);
+      });
+
+      const img = document.createElement("img");
+      img.src = pokemon.photo;
+      img.alt = pokemon.name;
+
+      detail.append(typesList, img);
+      listItem.append(number, name, detail);
+      pokemonList.appendChild(listItem);
+    });
   });
 }
 
-loadPokemonItens(offset, limit);
+loadPokemonItems(offset, limit);
 
 loadMoreButton.addEventListener("click", () => {
-  offset += limit
-  loadPokemonItens(offset, limit);
+  offset += limit;
+  loadPokemonItems(offset, limit);
 });
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
